fdad3740df6260d3b51c2394880cd626dbc4d172
Druk Oleksandr <druksasha@ukr.net>
2021-07-03 23:27:41 +0300

    minimal logging discord bot

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.taoensso/carmine "3.1.0"]]
+                 [com.taoensso/carmine "3.1.0"]
+                 [org.suskalo/discljord "1.2.3"]
+                 [org.clojure/core.async "1.3.618"]]
   :main ^:skip-aot schizoid.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/schizoid/core.clj
+++ src/schizoid/core.clj
@@ -1,14 +1,38 @@
 (ns schizoid.core
   (:require [schizoid.tokenizer :as token])
-  (:require [schizoid.trigram-repo :as trig])
   (:require [schizoid.reply-generator :as reply])
   (:require [schizoid.data-learner :as dlearner])
   (:require [schizoid.chance-repo :as chance])
+  (:require [schizoid.trigram-repo :as trig])
   (:require [clojure.string :as str])
-  ;; (:gen-class)
-  )
+  (:require [clojure.edn :as edn])
+  (:require [clojure.core.async    :as async])
+  (:require [discljord.connections :as conns])
+  (:require [discljord.messaging   :as mess])
+  (:gen-class))
+
+(def bot-token (-> "secrets.edn" slurp edn/read-string :bot-token))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [event-ch      (async/chan 100)
+        connection-ch (conns/connect-bot! bot-token event-ch :intents #{:guilds :guild-members :guild-bans :guild-emojis
+                                                                        :guild-integrations :guild-webhooks :guild-invites
+                                                                        :guild-voice-states :guild-presences :guild-messages
+                                                                        :guild-message-reactions :guild-message-typing
+                                                                        :direct-messages :direct-message-reactions
+                                                                        :direct-message-typing})
+        message-ch    (mess/start-connection! bot-token)]
+    (try
+      (loop []
+        (let [[event-type event-data] (async/<!! event-ch)]
+          (println "🎉 NEW EVENT! 🎉")
+          (println "Event type:" event-type)
+          (println "Event data:" (pr-str event-data))
+          (recur)))
+      (catch Exception e (str "Caught Exception: " (.getMessage e)))
+      (finally
+        (mess/stop-connection! message-ch)
+        (conns/disconnect-bot!  connection-ch)
+        (async/close!           event-ch)))))
+
