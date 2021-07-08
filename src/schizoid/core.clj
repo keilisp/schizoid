85a5346387abfd57f48b1744b528de01326b7c67
Druk Oleksandr <druksasha@ukr.net>
2021-07-08 18:53:52 +0300

    simple bot with declerative event handling

--- src/schizoid/core.clj
+++ src/schizoid/core.clj
@@ -1,38 +1,55 @@
 (ns schizoid.core
-  (:require [schizoid.tokenizer :as token])
-  (:require [schizoid.reply-generator :as reply])
-  (:require [schizoid.data-learner :as dlearner])
-  (:require [schizoid.chance-repo :as chance])
-  (:require [schizoid.trigram-repo :as trig])
-  (:require [clojure.string :as str])
-  (:require [clojure.edn :as edn])
-  (:require [clojure.core.async    :as async])
-  (:require [discljord.connections :as conns])
-  (:require [discljord.messaging   :as mess])
+  (:require [schizoid.tokenizer :as token]
+            [schizoid.reply-generator :as reply]
+            [schizoid.data-learner :as dlearner]
+            [schizoid.chance-repo :as chance]
+            [schizoid.trigram-repo :as trig]
+            [schizoid.message :as message]
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.core.async    :as async]
+            [discljord.connections :as conns]
+            [discljord.messaging   :as mess]
+            [discljord.events :as events])
   (:gen-class))
 
 (def bot-token (-> "secrets.edn" slurp edn/read-string :bot-token))
 
-(defn -main
-  [& args]
-  (let [event-ch      (async/chan 100)
-        connection-ch (conns/connect-bot! bot-token event-ch :intents #{:guilds :guild-members :guild-bans :guild-emojis
-                                                                        :guild-integrations :guild-webhooks :guild-invites
-                                                                        :guild-voice-states :guild-presences :guild-messages
-                                                                        :guild-message-reactions :guild-message-typing
-                                                                        :direct-messages :direct-message-reactions
-                                                                        :direct-message-typing})
-        message-ch    (mess/start-connection! bot-token)]
-    (try
-      (loop []
-        (let [[event-type event-data] (async/<!! event-ch)]
-          (println "🎉 NEW EVENT! 🎉")
-          (println "Event type:" event-type)
-          (println "Event data:" (pr-str event-data))
-          (recur)))
-      (catch Exception e (str "Caught Exception: " (.getMessage e)))
-      (finally
-        (mess/stop-connection! message-ch)
-        (conns/disconnect-bot!  connection-ch)
-        (async/close!           event-ch)))))
 
+(def state (atom nil))
+
+(defn greet-or-disconnect
+  [event-type {{bot :bot} :author :keys [channel-id content]}]
+  (if (= content "!disconnect")
+    (async/put! (:connection @state) [:disconnect])
+    (when-not bot
+      (mess/create-message! (:messaging @state) channel-id :content "Hello, World!"))))
+
+(defn send-emoji
+  [event-type {:keys [channel-id emoji]}]
+  (when (:name emoji)
+    (mess/create-message! (:messaging @state) channel-id
+                          :content (if (:id emoji)
+                                     (str "<:" (:name emoji) ":" (:id emoji) ">")
+                                     (:name emoji)))))
+
+(def handlers
+  {:message-create [#'greet-or-disconnect]
+   :message-reaction-add [#'send-emoji]})
+
+(let [event-ch (async/chan 100)
+      connection-ch (conns/connect-bot! bot-token event-ch :intents #{:guilds :guild-members :guild-bans :guild-emojis
+                                                                      :guild-integrations :guild-webhooks :guild-invites
+                                                                      :guild-voice-states :guild-presences :guild-messages
+                                                                      :guild-message-reactions :guild-message-typing
+                                                                      :direct-messages :direct-message-reactions
+                                                                      :direct-message-typing})
+      messaging-ch (mess/start-connection! bot-token)
+      init-state {:connection connection-ch
+                  :event event-ch
+                  :messaging messaging-ch}]
+  (reset! state init-state)
+  (try (events/message-pump! event-ch (partial events/dispatch-handlers #'handlers))
+       (finally
+         (mess/stop-connection! messaging-ch)
+         (conns/disconnect-bot! connection-ch))))
