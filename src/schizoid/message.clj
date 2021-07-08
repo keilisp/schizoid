f7638cc0293bb03e626fbd757ab88f79726bc4b2
Druk Oleksandr <druksasha@ukr.net>
2021-07-08 18:51:04 +0300

    +simple message handling funcs

---
+++ src/schizoid/message.clj
@@ -0,0 +1,118 @@
+(ns schizoid.message
+  (:require [clojure.string :as str]
+            [schizoid.chance-repo :as chance]))
+
+(def anchors #{"schizoid" "shizoid" "schiz" "shiz" "шиз" "шизоид" "шизик" "шизофреник"})
+
+;;;; Message objects
+
+;;; Just random message with text and mentions
+;; {:mention-everyone false,
+;;  :mentions [{:username "Tance",
+;;              :public-flags 0,
+;;              :member {:roles [],
+;;                       :mute false,
+;;                       :joined-at "2021-05-21T16:29:32.855000+00:00",
+;;                       :hoisted-role nil,
+;;                       :deaf false},
+;;              :id "593186395246428161",
+;;              :discriminator "9673",
+;;              :avatar "9e53066f021e224d63e072f2cec16045"}
+;;             {:username "mediocreBot",
+;;              :public-flags 0,
+;;              :member {:roles ["839645752523358250"],
+;;                       :mute false,
+;;                       :joined-at "2021-05-05T23:32:27.389000+00:00",
+;;                       :hoisted-role nil,
+;;                       :deaf false},
+;;              :id "615126051680681994",
+;;              :discriminator "8020",
+;;              :bot true,
+;;              :avatar "9db02128c4cdb60212aae416b63f0d0b"}
+;;             {:username "Schizoid",
+;;              :public-flags 0,
+;;              :member {:roles [],
+;;                       :mute false,
+;;                       :joined-at "2021-05-21T17:27:38.284000+00:00",
+;;                       :hoisted-role nil,
+;;                       :deaf false},
+;;              :id "845341783965565028",
+;;              :discriminator "0904",
+;;              :bot true,
+;;              :avatar "0b8e2600823f411510d2e5f2099ff135"}],
+;;  :pinned false,
+;;  :content "test message with menitons: <@!593186395246428161> <@!615126051680681994> , threads: <#839645600798343192> , mention of bot itself <@!845341783965565028> , slash commands: /test /qwer , bot aliases: shiz шизоид шиз",
+;;  :attachments [],
+;;  :mention-roles [],
+;;  :type 0,
+;;  :referenced-message nil,
+;;  :guild-id "839645600798343188",
+;;  :author {:username "M-x 𝜆",
+;;           :public-flags 0,
+;;           :id "399637644843155457",
+;;           :discriminator "1471",
+;;           :avatar "75ba0a3639e2a5b614d44005c76ee0fc"},
+;;  :member {:roles [],
+;;           :mute false,
+;;           :joined-at "2021-05-05T23:31:51.146000+00:00",
+;;           :hoisted-role nil,
+;;           :deaf false},
+;;  :components [],
+;;  :id "862644490321788948",
+;;  :channel-id "839645600798343192",
+;;  :embeds [],
+;;  :timestamp "2021-07-08T10:41:13.652000+00:00",
+;;  :flags 0,
+;;  :nonce "862644489364176896",
+;;  :tts false,
+;;  :edited-timestamp nil
+
+(defn has-text?
+  [message]
+  (let [text (:content message)]
+    (and (some? text) (not (str/blank? text)))))
+
+(defn is-sticker?
+  [message]
+  (let [stickers (:sticker_itmes message)]
+    (and (some? stickers) (not (empty? stickers)))))
+
+(defn was-edited?
+  [message]
+  (some? (:edited-timestamp message)))
+
+(defn has-mentions?
+  [message]
+  (not (empty? (:mentions message))))
+
+(defn has-attachments?
+  [message]
+  (not (empty? (:attachments message))))
+
+(defn has-anchors?
+  [message]
+  (let [text (:content message)
+        words-vec (str/split (str/trim text) #" ")
+        mentions (map #(:username %) (:mentions message))]
+    (or (some #{"Sсhizoid"} mentions)
+        (->> anchors
+             (map #(str/includes? text %))
+             (some #{true})))))
+
+;; TEST
+(defn is-reply-to-bot?
+  [message]
+  (let [referenced-message (:referenced-message message)]
+    (and (some? referenced-message)
+         (= "Schizoid" (:username (:author referenced-message))))))
+
+;; FIXME
+(defn is-random-answer?
+  [message]
+  (< (rand-int) 100) (chance/get-chance (:guild-id message)))
+
+(defn should-answer?
+  [message]
+  (or (has-anchors? message)
+      (is-reply-to-bot? message)
+      (is-random-answer? message)))
---
+++ src/schizoid/message_handler.clj
@@ -0,0 +1,33 @@
+(ns schizoid.message-handler
+  (:require [schizoid.reply-generator :as reply]
+            [schizoid.data-learner :as dlearner]
+            [schizoid.chance-repo :as chance]
+            [schizoid.message :as message]
+            [discljord.messaging   :as mess]))
+
+(defn process-message
+  [conn chat-id message]
+  (let [should-answer? (message/should-answer? message)]
+    (when should-answer? (mess/trigger-typing-indicator! conn chat-id))
+    (dlearner/learn message)
+    (when should-answer?
+      (when-let [reply-text (reply/generate message)]
+          (mess/create-message! conn chat-id :content reply-text)))))
+
+(defn process-sticker
+  [conn chat-id message]
+  (when (or (message/has-anchors? message)
+            (message/is-reply-to-bot? message))
+    (mess/create-message! conn chat-id :content "sticker")))
+
+(defn handle
+  [conn message]
+  (let [chat-id (:guild-id message) 
+        chance (chance/get-chance chat-id)]
+    (if (and (message/has-text? message)
+             (message/was-edited? message))
+      (process-message conn chat-id message)
+      (when (message/is-sticker? message)
+        (process-sticker conn chat-id message)))))
+
+
