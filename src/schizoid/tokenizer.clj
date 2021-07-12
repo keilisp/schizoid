f749480f6cbe3de5e723b8aea55e4b5737562e1e
Druk Oleksandr <druksasha@ukr.net>
2021-07-12 13:35:03 +0300

    +tokenizer tests

--- src/schizoid/tokenizer.clj
+++ src/schizoid/tokenizer.clj
@@ -2,11 +2,14 @@
   (:require [clojure.string :as str]
             [clojure.edn :as edn]))
 
-(def chain-len (-> "config.edn" slurp edn/read-string :grammar :chain-len))
-(def stop-word (-> "config.edn" slurp edn/read-string :grammar :stop-word))
+(def config (-> "config.edn" slurp edn/read-string))
 
-(def endsen (-> "config.edn" slurp edn/read-string :grammar :endsen))
-(def garbage (-> "config.edn" slurp edn/read-string :grammar :garbage))
+(def chain-len (-> config :grammar :chain-len))
+(def stop-word (-> config :grammar :stop-word))
+
+(def endsen (-> config :grammar :endsen))
+(def garbage (-> config :grammar :garbage))
+
 
 (defn append-stop-words
   "Append `stop-word` to the `words` after the end of the sentence."
@@ -35,12 +38,14 @@
   "Lowercase and remove `garbage` from word."
   [word]
   (let [lowercased (str/lower-case word)
-        last-char (if (not (some #(= (last word) %) endsen))
+        last-char (if-not (some #(= (last word) %) endsen)
                     ""
                     (last lowercased))
-        prettified (str/join (remove garbage lowercased))]
-    ;; FIXME what is 2 and why to > and not >=
-    (if (and (seq prettified) (> (count prettified) 2))
+        prettified (->> lowercased
+                        (remove garbage)
+                        str/join)]
+    (if (and (seq prettified)
+             (> (count prettified) 2))
       (str prettified last-char)
       nil)))
 
@@ -49,16 +54,21 @@
   [message]
   (let [words (str/split message #" ")
 
-        ;; FIXME
+        ;; FIXME 
         url-pattern #"(?i)^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$"
 
         email-pattern #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
 
-        user-mention-pattern #"<@!\d*>"
-        channel-mention-pattern #"<#\d*>"
+        user-mention-pattern #"<@!\d+>"
+        channel-mention-pattern #"<#\d+>"
+        ;; FIXME probably would be cool not to trim emotes and store pairs with them
+        emotes-pattern #":\S+:"
+        slash-commands-pattern #"/\S+"
 
         words-without-entities (filter #(not (or (re-matches user-mention-pattern %)
                                                  (re-matches channel-mention-pattern %)
+                                                 (re-matches emotes-pattern %)
+                                                 (re-matches slash-commands-pattern %)
                                                  (re-matches email-pattern %)
                                                  (re-matches url-pattern %))) words)]
 
---
+++ test/schizoid/tokenizer_test.clj
@@ -0,0 +1,110 @@
+(ns schizoid.tokenizer-test
+  (:require [clojure.test :refer :all]
+            [schizoid.tokenizer :refer :all]))
+
+(deftest prettify-word-test
+  (testing "prettify-word"
+    (is (nil? (prettify-word "te")))
+    (is (nil? (prettify-word "t")))
+    (is (nil? (prettify-word "")))
+    (is (= (prettify-word "  «({[* & ;; <t!e\t#s\nt-w№o$r:^d... ]})»  ? </ \\ | | / !")
+           "test-word!"))))
+
+(deftest extract-words-test
+  (testing "extract-words"
+    (is (= [] (extract-words "")))
+    (is (= ["test"
+            "message"
+            "with"
+            "menitons"
+            "threads"
+            "emotes"
+            "slash"
+            "commands"
+            "links"
+            "emails"
+            "words"
+            "with"
+            "garbage"
+            "bla-bla-bla"]
+           (extract-words "test message with menitons: <@!593186395246428161> , threads: <#839645600798343192> , emotes: :FeelsGayMan: , slash commands: /test /qwer , links: https://www.fsf.org/ , emails: blabla@test.com , words with garbage: bl#$a)-bl!.a-bl^a")))))
+
+(deftest append-stop-words-test
+  (testing "append-stop-words"
+    (is (= [] (append-stop-words [])))
+    (is (= ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great"]
+           (append-stop-words ["hello" "world!" "how" "are" "you" "all" "doing?" "great"])))
+    (is (= ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great." "0"]
+           (append-stop-words ["hello" "world!" "how" "are" "you" "all" "doing?" "great."])))))
+
+(deftest stop-word-to-end-test
+  (testing "stop-word-to-end"
+    (is (= ["0"] (stop-word-to-end [])))
+    (is (= ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great" "0"]
+           (stop-word-to-end ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great"])))
+    (is (= ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great" "0"]
+           (stop-word-to-end ["hello" "world!" "0" "how" "are" "you" "all" "doing?" "0" "great" "0"])))))
+
+(deftest split-to-trigrams-test
+  (testing "split-to-trigrams"
+    (is (= [] (split-to-trigrams [])))
+    (is (= [["test" "message" "with"]
+            ["message" "with" "mentions"]
+            ["with" "mentions" "threads"]
+            ["mentions" "threads" "emotes"]
+            ["threads" "emotes" "slash"]
+            ["emotes" "slash" "commands"]
+            ["slash" "commands" "links"]
+            ["commands" "links" "emails"]
+            ["links" "emails" "words"]
+            ["emails" "words" "with"]
+            ["words" "with" "garbage"]
+            ["with" "garbage" "bla-bla-bla"]
+            ["garbage" "bla-bla-bla" "0"]]
+
+           (split-to-trigrams ["test"
+                               "message"
+                               "with"
+                               "mentions"
+                               "threads"
+                               "emotes"
+                               "slash"
+                               "commands"
+                               "links"
+                               "emails"
+                               "words"
+                               "with"
+                               "garbage"
+                               "bla-bla-bla"])))
+
+    (is (= [["test" "message" "with"]
+            ["message" "with" "mentions!"]
+            ["with" "mentions!" "0"]
+            ["mentions!" "0" "threads"]
+            ["0" "threads" "emotes"]
+            ["threads" "emotes" "slash"]
+            ["emotes" "slash" "commands."]
+            ["slash" "commands." "0"]
+            ["commands." "0" "links"]
+            ["0" "links" "emails?"]
+            ["links" "emails?" "0"]
+            ["emails?" "0" "words"]
+            ["0" "words" "with"]
+            ["words" "with" "garbage"]
+            ["with" "garbage" "bla-bla-bla"]
+            ["garbage" "bla-bla-bla" "0"]]
+
+           (split-to-trigrams ["test"
+                               "message"
+                               "with"
+                               "mentions!"
+                               "threads"
+                               "emotes"
+                               "slash"
+                               "commands."
+                               "links"
+                               "emails?"
+                               "words"
+                               "with"
+                               "garbage"
+                               "bla-bla-bla"])))))
